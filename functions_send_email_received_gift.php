8ecdccb36fcfa4d4fc41edb009c672cc08e01fb5
Elton Pereira <eltondeveloper@gmail.com>
2015-05-04 13:57:28 -0300

    version 0.1

--- functions_send_email_received_gift.php
+++ functions_send_email_received_gift.php
@@ -7,9 +7,6 @@
  * Author URI: http://eltondev.com.br
  * Version: 0.1
  *
- * License: GNU General Public License v3.0
- * License URI: http://www.gnu.org/licenses/gpl-3.0.html
- *
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
